6c5a1e37969b5e6289245be5251ed700101fc290
dnema09 <dnema09@gmail.com>
2022-01-26 02:33:32 +0530

    build commit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/dnema09/react-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out my first react code..
         </a>
       </header>
     </div>
